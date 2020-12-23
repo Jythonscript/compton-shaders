8a19aea9d92797946cbd228dc3ec681f4e44ea47
Jythonscript <bartnik314@gmail.com>
2020-12-23 12:30:49 -0600

    remove comment

--- compton-fake-transparency-fshader-win.glsl
+++ compton-fake-transparency-fshader-win.glsl
@@ -7,8 +7,7 @@ void main() {
 	if (!invert_color) { // Hack to allow compton exceptions
 		// Change the vec4 to your desired key color
 		//vec4 vdiff = abs(vec4(0.0039, 0.0039, 0.0039, 1.0) - c); // #010101
-		vec4 vdiff = abs(vec4(0.0000, 0.0039, 0.0078, 1.0) - c); // #000102
-		//vec4 vdiff = abs(vec4(0.0039, 0.0039, 0.0039, 1.0) - c); // #010101
+		vec4 vdiff = abs(vec4(0, 0.0039, 0.0078, 1.0) - c); // #000102
 		float diff = max(max(max(vdiff.r, vdiff.g), vdiff.b), vdiff.a);
 		// Change the vec4 to your desired output color
 		if (diff < 0.001)
